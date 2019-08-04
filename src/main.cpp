f20bcba309715d714df561f6b06a4ea89a382116
Krzysiek Kopacz <krzysiek-kopacz@o2.pl>
2019-08-04 13:21:00 +0200

    Change to CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
-#include "CourseDashboard.hpp"
-
+#include "UserData.hpp"
+#include <unistd.h>
 int main()
 {
-    std::cout << "Course Dashboard" << std::endl;
+
+    std::cout << "Menu: " << std::endl;
+    std::cout << "1. Dodaj uczestnika" << std::endl;
+    int wyborOpcji;
+    std::cin >> wyborOpcji;
+    
+               UserData user;
+    switch(wyborOpcji)
+    {
+        case 1: 
+               user.setName("Krzysiek");
+               user.setNick("Cypher");
+               user.setFirecodeLink("https://www.testowy-link.og");
+               user.setGithubLink("https://www.kolejny-test.og");
+                
+    }
+
+   // sleep(3000);
+    std::cout << "Obecni uczestnicy kursu: " << std::endl;
+    std::cout << "Imie: " << user.getName() << std::endl;
+    std::cout << "Nick: " << user.getNick() << std::endl;
+    std::cout << "FirecodeLink: " << user.getFirecodeLink() << std::endl;
+    std::cout << "GithubLink: " << user.getGithubLink() << std::endl;
+
     return 0;
 }
