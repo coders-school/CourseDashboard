1c1ffffd2e10c705cc7e03ec9d41b33e39dcc675
TheDude-cpu <dudek.u.sylwia@gmail.com>
2019-08-07 22:50:28 +0200

    repair function for trainer

--- src/Trainer.hpp
+++ src/Trainer.hpp
@@ -3,8 +3,25 @@
 
 class Trainer : public virtual coders
 {
-    protected:
-
     public:
-
+    void Coders::insertName(std::string tmpName) 
+    {
+        name = tmpName;
+    }
+    void Coders::insertDiscordNickname(std::string tmpDiscord) 
+    {
+        discordNickname = tmpDiscord;
+    }
+    void Coders::insertGithubLink(std::string tmpGit) 
+    {
+        githubLink = tmpGit;
+    }
+    void Coders::insertFirecodeLink(std::string tmpFire) 
+    {
+        firecodeLink = tmpFire;
+    }
+    void Coders::insertGroup(std::string tmpGroup) 
+    {
+        group = tmpGroup;
+    }
 };
