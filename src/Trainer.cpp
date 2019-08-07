7d7e14562fcffa9263164f8d2370f6fd5b46dec6
TheDude-cpu <dudek.u.sylwia@gmail.com>
2019-08-07 22:40:19 +0200

    function for trainer

--- src/Trainer.cpp
+++ src/Trainer.cpp
@@ -1,2 +1,28 @@
 #include "Trainer.hpp"
 #include <iostream>
+
+void Coders::insertName(std::string tmpName) 
+{
+    name = tmpName;
+    std::cout << name;
+}
+void Coders::insertDiscordNickname(std::string tmpDiscord) 
+{
+    discordNickname = tmpDiscord;
+    std::cout << discordNickname;
+}
+void Coders::insertGithubLink(std::string tmpGit) 
+{
+    githubLink = tmpGit;
+    std::cout << githubLink;
+}
+void Coders::insertFirecodeLink(std::string tmpFire) 
+{
+    firecodeLink = tmpFire;
+    std::cout << firecodeLink;
+}
+void Coders::insertGroup(std::string tmpGroup) 
+{
+    group = tmpGroup;
+    std::cout << group;
+}
