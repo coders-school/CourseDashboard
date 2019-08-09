e5685950587aca033e20e7bedb8291bc451535ff
Wiktor Lasota <wiktor.lasota@vp.pl>
2019-08-09 20:29:07 +0200

    Revert "original+mine"
    This reverts commit a471af0a75e9ffe00a21e2dbd6e0fb46bedb0ec3.

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,8 @@
-#include <Trainer.hpp>
-#include <Student.hpp>
 #include <iostream>
 #include "CourseDashboard.hpp"
 
 int main()
 {
     std::cout << "Course Dashboard" << std::endl;
-     Trainer trainer1;
-     trainer1.addUser("Wiktor","Lasota","Arakis14");
-     trainer1.addUser("Jan","Kowalski","kowal1234");
-     trainer1.addUser("Test","something ","anything");
-     std::cout<<"=============================="<<std::endl;
-     trainer1.viewUsers();
-     trainer1.deleteUser(0);
-     trainer1.deleteUser(1);
-
-     return 0;
- }
+    return 0;
+}
