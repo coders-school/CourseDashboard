021213b8f319d0583b4cc0ef91412b1d7bb02f1b
Wiktor Lasota <wiktor.lasota@vp.pl>
2019-08-09 20:30:35 +0200

    Revert "Project_version_1"
    This reverts commit 8cfd107035d917bcd310da63b4f583b66fc15460.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,8 @@
 #include <iostream>
-#include <Coach.hpp>
-#include <Student.hpp>
-
+#include "CourseDashboard.hpp"
 
 int main()
 {
-    Trainer trainer1;
-    Trainer1.addUser("Wiktor","Lasota","Arakis14");
-    Trainer1.addUser("Jan","Kowalski","kowal1234");
-    Trainer1.addUser("Test","something ","anything");
-    std::cout<<"=============================="<<std::endl;
-    Trainer1.viewUsers();
-    Trainer1.deleteUser(0);
-    Trainer1.deleteUser(1);
-
+    std::cout << "Course Dashboard" << std::endl;
     return 0;
 }
