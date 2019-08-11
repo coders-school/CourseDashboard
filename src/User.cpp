af82a0cded6f5970743347c3c50ddb0195328554
alicjaliQui <alicjaliqui@gmail.com>
2019-08-11 23:58:22 +0200

    added firecode info

--- src/User.cpp
+++ src/User.cpp
@@ -2,7 +2,7 @@
 
 void User::printUserdata()
 {
-    std::cout << name << " " << surname << " " << discord << " " << github << std::endl;
+    std::cout << name << " " << surname << " " << discord << " " << github << " " << firecode << std::endl;
 }
 
 void User::deleteUser()
@@ -11,12 +11,14 @@ void User::deleteUser()
     surname = "";
     discord = "";
     github = "";
+    firecode = "";
 }
 
-void User::changeData(std::string a, std::string b, std::string c, std::string d)
+void User::changeData(std::string a, std::string b, std::string c, std::string d, std::string e)
 {
     name = a;
     surname = b;
     discord = c;
     github = d;   
+    firecode = e;
 }
--- src/User.hpp
+++ src/User.hpp
@@ -7,14 +7,15 @@ class User
     public:
     // Constructors
     User(){};    
-    User(std::string name, std::string surname, std::string discord, std::string github):
-    name(name), surname(surname), discord(discord), github(github) {}
+    User(std::string name, std::string surname, std::string discord, std::string github, std::string firecode):
+    name(name), surname(surname), discord(discord), github(github), firecode(firecode) {}
     User(std::vector<std::string> wek)
     {
         name = wek[0];
         surname = wek[1];
         discord = wek[2];
         github = wek[3];
+        firecode = wek[4];
     }
 
     // Members
@@ -22,6 +23,7 @@ class User
     std::string surname;
     std::string discord;
     std::string github;
+    std::string firecode;
 
     // Methods
     // --> print user data
@@ -29,7 +31,7 @@ class User
     // --> delete user
     void deleteUser();
     // --> change user data
-    void changeData(std::string a, std::string b, std::string c, std::string d);
+    void changeData(std::string a, std::string b, std::string c, std::string d, std::string e);
     
     // Destructor
     ~User(){};
