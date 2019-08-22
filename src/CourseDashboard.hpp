82fd9faecefef1cb142d0f44e22e54d043ff3ace
darkassazi <darkassazi@gmail.com>
2019-08-22 12:00:28 +0200

    Extract all US1 requirements to another class

--- src/CourseDashboard.hpp
+++ src/CourseDashboard.hpp
@@ -1,19 +1,7 @@
 #pragma once
-#include "User.hpp"
-#include <vector>
-
 
 class CourseDashboard
 {
 public:
     CourseDashboard();
-    void showAll();
-    void createUser(const User & user);
-    void deleteUserByNick(std::string nick);
-    void retriveUserByNick(std::string nick);
-    void updateUser(User & user);
-
-private:
-    std::vector<User> users_;
-
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
+
 #include <iostream>
+
 #include "CourseDashboard.hpp"
+#include "User.hpp"
+#include "UserContainer.hpp"
 
 
 int main()
 {
-    CourseDashboard cdb;
+    UserContainer uc;
     std::cout << "Course Dashboard" << std::endl;
     User u1("Szymon", "SzymonGajewski", "wieczorowa", "SzymonGajewski", "SzymonGajewski");
-    cdb.createUser(u1);
-    cdb.retriveUserByNick("SzymonGajewski");
-    cdb.showAll();
-    cdb.deleteUserByNick("SzymonGajewski");
+    uc.createUser(u1);
+    uc.retriveUserByNick("SzymonGajewski");
+    uc.showAll();
+    uc.deleteUserByNick("SzymonGajewski");
     std::cout<<"after delete: "<<std::endl;
-    cdb.showAll();
+    uc.showAll();
     return 0;
 }
