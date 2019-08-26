8b7408e5d60b0ff115da211db22d5479cc6497b3
Piotr Hoffner <piotr.hoffner.wroc@gmail.com>
2019-08-26 12:13:18 +0200

    created some users in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,11 @@ int main()
 {
     CourseDashboard cdb;
     std::cout << "Course Dashboard" << std::endl;
-    User ph("ph1", "ph2", "ph3", "ph4", "ph5", "ph6", "ph7");
+    User ng("ngName", "ngNick", "ngPassword", "ngMail", "ngGroup", "ngGithub", "ngFirecode");
+    User rh("rhName", "rhNick", "rhPassword", "rhMail", "rhGroup", "rhGithub", "rhFirecode");
+    User ph("phName", "phNick", "phPassword", "phMail", "phGroup", "phGithub", "phFirecode");
+    cdb.createUser(ng);
+    cdb.createUser(rh);
     cdb.createUser(ph);
     cdb.showAll();
     return 0;
