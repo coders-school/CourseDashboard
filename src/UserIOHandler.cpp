c6fe5ead0c9fa67978fd176154dfdd0357ae0406
alicjaliQui <alicjaliqui@gmail.com>
2019-08-27 19:28:56 +0200

    Added read methos in UserIOHandler; added test

--- src/UserIOHandler.cpp
+++ src/UserIOHandler.cpp
@@ -5,8 +5,10 @@
 #include <ios>
 #include <iostream>
 #include <memory>
+#include <string>
 
 using fstreamPtr = std::unique_ptr<std::fstream, std::function<void(std::fstream*)>>;
+using ifstreamPtr = std::unique_ptr<std::ifstream>;
 
 void UserIOHandler::write(const std::string& filePath, const std::string& strBuff)
 {
@@ -29,6 +31,26 @@ void UserIOHandler::write(const std::string& filePath, const std::string& strBuf
     catch(std::ios_base::failure& e)
     {
         std::cerr << "Write to file: " << filePath << "failed.\n"
+            << "Error message: " << e.what() << "\n";
+    }
+}
+
+void UserIOHandler::read(const std::string& filePath, std::string& strBuff)
+{
+    ifstreamPtr file(new std::ifstream());
+
+    try
+    {
+        file->open(filePath, std::fstream::out);
+        while(!file->eof()) {
+            strBuff.push_back(file->get());
+        }
+        strBuff.pop_back();
+    }
+
+    catch(std::ios_base::failure& e)
+    {
+        std::cerr << "Read from file: " << filePath << "failed.\n"
             << "Error message: " << e.what() << "\n";
     }
 }
