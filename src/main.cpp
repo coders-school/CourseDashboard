3ad4d32562ff5211aed8b4bd98ba288cce734d97
TheDude-cpu <dudek.u.sylwia@gmail.com>
2019-08-27 21:32:31 +0200

    change user

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,11 @@ int main()
     CourseDashboard cdb;
     std::cout << "Course Dashboard" << std::endl;
     cdb.logIn();
-    User u1("Szymon", "SzymonGajewski", "wieczorowa", "SzymonGajewski", "SzymonGajewski");
+    User u1("Jan", "JanKowalsky", "wieczorowa", "JanKowalsky", "JanKowalsky");
     cdb.createUser(u1);
-    cdb.retriveUserByNick("SzymonGajewski");
+    cdb.retriveUserByNick("JanKowalsky");
     cdb.showAll();
-    cdb.deleteUserByNick("SzymonGajewski");
+    cdb.deleteUserByNick("JanKowalsky");
     std::cout<<"after delete: "<<std::endl;
     cdb.showAll();
     return 0;
