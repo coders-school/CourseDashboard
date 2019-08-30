33c2cf38a392b9e623829986440261f3ae12b1e0
TheDude-cpu <dudek.u.sylwia@gmail.com>
2019-08-30 08:54:37 +0200

    first tests

--- tests/CourseDashboardTests.cpp
+++ tests/CourseDashboardTests.cpp
@@ -1,8 +1,33 @@
 #include <gtest/gtest.h>
+#include <string>
 #include "CourseDashboard.hpp"
 
+struct TestLogIn : public testing::Test
+{
+    CourseDashboard login;
+};
 
 TEST(CourseDashboardTests, canConstructCourseDashboard)
 {
     CourseDashboard cd;
 }
+
+TEST_F(TestLogIn, tryEmailReturnTrue)
+{
+    CourseDashboard login();
+}
+
+TEST_F(TestLogIn, EmailReturnTrue)
+{
+    auto result = login.provideEmail("testForEmail");
+
+    ASSERT_TRUE(result);
+}
+
+TEST_F(TestLogIn, PasswordReturnTrue)
+{
+    auto result = login.providePassword("testForEmail");
+
+    ASSERT_TRUE(result);
+}
+
