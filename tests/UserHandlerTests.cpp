529cc934114d9aa8d363709ece200bbc378592d3
Łukasz Ziobroń <lukasz@coders.school>
2019-09-07 12:55:45 +0200

    Test:retriveNotExistingUserReturnsNullptr added + refactor

--- tests/UserHandlerTests.cpp
+++ tests/UserHandlerTests.cpp
@@ -2,17 +2,26 @@
 #include "UserHandler.hpp"
 #include "User.hpp"
 
-TEST(UserHandlerTests, retriveUserFromEmptyDatabaseReturnsNullptr)
-{
-    UserHandler cut;
-    ASSERT_EQ(cut.retriveUserByNick(""), nullptr);
-}
-
-TEST(UserHandlerTests, retriveUserFromDatabaseReturnsRequestedUser)
+struct UserHandlerTests : public testing::Test
 {
     UserHandler cut;
     std::string nick = "SzymonG";
-    User szymon("Szymon", nick, User::Group::evening, "SzymonG", "SzymonG");
+    User szymon {"Szymon", nick, User::Group::evening, "SzymonG", "SzymonG"};
+};
+
+TEST_F(UserHandlerTests, retriveUserFromEmptyDatabaseReturnsNullptr)
+{
+    ASSERT_EQ(cut.retriveUserByNick(""), nullptr);
+}
+
+TEST_F(UserHandlerTests, retriveUserFromDatabaseReturnsRequestedUser)
+{
     cut.createUser(szymon);
     ASSERT_EQ(*cut.retriveUserByNick(nick), szymon);
+}
+
+TEST_F(UserHandlerTests, retriveNotExistingUserReturnsNullptr)
+{
+    cut.createUser(szymon);
+    ASSERT_EQ(cut.retriveUserByNick("nonExisting"), nullptr);
 }
