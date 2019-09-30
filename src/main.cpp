e877e317366272675d8bdd9e5f295ab38de46ba1
Łukasz Ziobroń <lukasz@coders.school>
2019-09-30 13:51:17 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -29,8 +29,8 @@ int main()
 
     User user1("joanna", "bed", User::Group::evening, "teo", "fire", "email", "12345");
     std::cout << user1.getPassword() << std::endl;
-    AuthenticationProvider a;
-    std::cout << std::boolalpha << a(user1, "email", "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5") << std::endl;
+    AuthenticationProvider ap;
+    std::cout << std::boolalpha << ap.authenticate(user1, "email", "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5") << std::endl;
 
     return 0;
 }
